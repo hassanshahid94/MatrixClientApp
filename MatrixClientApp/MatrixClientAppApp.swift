b62789ed52b8cfc54c6be125341d378b158def97
Hassan Shahid <hassanshahid@Mac.home>
2025-11-22 14:02:47 +0200

    App created

---
+++ MatrixClientApp/MatrixClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixClientAppApp.swift
+//  MatrixClientApp
+//
+//  Created by Hassan Shahid on 22.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MatrixClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatrixClientAppTests/MatrixClientAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixClientAppTests.swift
+//  MatrixClientAppTests
+//
+//  Created by Hassan Shahid on 22.11.2025.
+//
+
+import Testing
+@testable import MatrixClientApp
+
+struct MatrixClientAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
